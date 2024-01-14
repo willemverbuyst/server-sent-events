46a23c202b8233ad2fa4c3e58d4af251b00db28e
Willem Verbuyst <willemverbuyst@gmail.com>
2024-01-14 12:24:31 +0100

    Add cors config on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import cors from "@fastify/cors";
 import fastify from "fastify";
 import pino from "pino";
 import pretty from "pino-pretty";
@@ -5,6 +6,16 @@ import pretty from "pino-pretty";
 export const server = fastify({
   logger: pino(pretty()),
 });
+
+const corsOptions = {
+  origin: "*",
+  methods: "OPTION,GET,HEAD,PUT,PATCH,DELTE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  exposeHeaders: "Authorization",
+};
+
+server.register(cors, corsOptions);
 
 server.get("/", async function handler() {
   return { msg: "root" };
