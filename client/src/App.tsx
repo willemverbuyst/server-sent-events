11dff020c5c8f6409c76ecaca3e46d9c375e120f
Willem Verbuyst <willemverbuyst@gmail.com>
2024-01-14 06:26:45 +0100

    Set up simple server

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <section
+      style={{ display: "flex", width: "inherit", justifyContent: "center" }}
+    >
+      <h1>Server-Sent Events</h1>
+    </section>
+  );
+}
+
+export default App;
